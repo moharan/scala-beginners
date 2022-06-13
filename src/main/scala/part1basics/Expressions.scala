355fdd4a692827f1a07e3b22d685a5a8819280d6
Rangi Becerra <rangibecerra@gmail.com>
2022-06-12 21:59:44 -0400

    section 2, expressions

---
+++ src/main/scala/part1basics/Expressions.scala
@@ -0,0 +1,14 @@
+package part1basics
+
+object Expressions extends App {
+
+  // expression
+  val x = 1 + 2
+  println(x)
+
+  // mathematic expression
+  println(2+4*5)
+
+  // boolean
+  println(3 == x)
+}
